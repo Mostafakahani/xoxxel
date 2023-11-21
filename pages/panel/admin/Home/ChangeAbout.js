312f4fae01b7c77c89d67431b5acf62a9c0d40e1
Mostafa <kahanimostafa83@gmail.com>
2023-11-21 22:09:52 +0330

    change about

--- pages/panel/admin/Home/ChangeAbout.js
+++ pages/panel/admin/Home/ChangeAbout.js
@@ -1,8 +1,27 @@
 import { Button, Grid } from "@mui/material";
 import StepAbout from "Components/Common/HomePageSteps/FAQ/StepAbout";
+import ServerURL from "Components/Common/Layout/config";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 const ChangeAbout = () => {
+    const [count, setCount] = useState(0)
+    const [data, setData] = useState('')
 
-
+    const [question, setQuestion] = useState('')
+    const [text, setText] = useState('')
+    
+    useEffect(() => {
+        async function fetchData() {
+            const config = { headers: { Authorization: `${ServerURL.Bear}` } };
+            const response = await axios.get(
+                `${ServerURL.url}/admin/info/get-about-us`,
+                config
+            );
+            setData(response.data.data);
+        }
+        fetchData();
+    }, [count]);
 
 
     return (
@@ -10,10 +29,14 @@ const ChangeAbout = () => {
 
             <Grid sx={{ backgroundColor: '#fff', p: '25px' }}>
                 <Grid>
-                    <StepAbout />
+                    <StepAbout
+                        sendQuestion={(e) => setQuestion(e)}
+                        sendText={(e) => setText(e)}
+                        getTitel={''}
+                    />
                 </Grid>
                 <Grid sx={{ my: '20px' }}>
-                    <Button variant="contained" disableElevation sx={{ borderRadius: '5px', backgroundColor: '#1C49F1', color: '#FFFFFF' }}>ذخیره تغییرات</Button>
+                    <Button onClick={() => console.log(question, text)} variant="contained" disableElevation sx={{ borderRadius: '5px', backgroundColor: '#1C49F1', color: '#FFFFFF' }}>ذخیره تغییرات</Button>
                 </Grid>
             </Grid>
 
--- src/Components/Common/HomePageSteps/FAQ/StepAbout.jsx
+++ src/Components/Common/HomePageSteps/FAQ/StepAbout.jsx
@@ -1,12 +1,46 @@
-import { Accordion, AccordionDetails, AccordionSummary, Grid, TextField, Typography } from "@mui/material";
-import { useState } from "react";
+import { Accordion, AccordionDetails, AccordionSummary, Grid, TextField, Typography, Button } from "@mui/material";
+import ServerURL from "Components/Common/Layout/config";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-const StepAbout = () => {
-    const [titel, setQuestion] = useState('')
+const StepAbout = ({ getTitel, des, sendQuestion = () => { }, sendText = () => { } }) => {
+    const [question, setQuestion] = useState('')
     const [text, setText] = useState('')
 
-    const handleSubmit = () => {
 
+
+
+
+    const handleSubmit = async () => {
+        sendQuestion(question)
+        sendText(text)
+        // try {
+        //     const config = {
+        //         headers: {
+        //             Authorization: `${ServerURL.Bear}`,
+        //         },
+        //     };
+
+        //     const newData = {
+        //         title: question,
+        //         description: text,
+        //     };
+
+        //     const response = await axios.post(
+        //         `${ServerURL.url}/admin/info/about-us/create`,
+        //         newData,
+        //         config
+        //     );
+        //     if (response.data.status === "success") {
+        //         toast.success("با موفقیت حذف شد.");
+        //         setCount(count + 1);
+        //     } else {
+        //         toast.error("لطفا دوباره امتحان کنید");
+        //     }
+        // } catch (error) {
+        //     console.error("Error sending delete request:", error);
+        // }
     };
 
     return (
@@ -21,7 +55,7 @@ const StepAbout = () => {
                                     // error={!priceError ? false : true}
                                     // helperText={!priceError ? '' : ErrorList[1]}
                                     onChange={(e) => setQuestion(e.target.value)}
-                                    value={titel}
+                                    value={question}
                                     label="عنوان  "
                                     variant="outlined"
                                     sx={{ width: { xs: '100%', sm: '100%', md: '100%' }, my: '10px' }}
@@ -44,6 +78,7 @@ const StepAbout = () => {
                             </Grid>
                         </Grid>
                     </Grid>
+                    <Button onClick={() => handleSubmit()}>Log</Button>
                 </Grid>
             </Grid>
         </>
