64d5971d8263f36ad641082fd2ece6822e239e7c
Mostafa <kahanimostafa83@gmail.com>
2023-11-30 17:00:33 +0330

    added chat message

--- src/Components/Common/Chat/ChatMessages.jsx
+++ src/Components/Common/Chat/ChatMessages.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Avatar, Box, Typography } from '@mui/material';
 
 const ChatMessages = () => {
 
@@ -8,16 +8,22 @@ const ChatMessages = () => {
             <Box sx={{
                 display: 'flex',
                 justifyContent: 'flex-end',
-                p: 1
+                p: 1,
             }}>
                 <Typography sx={{
-                    bgcolor: 'primary.main',
-                    color: 'white',
-                    p: 1,
-                    borderRadius: 1
+                    bgcolor: '#5094FB',
+                    color: '#FFFFFF',
+                    px: 2,
+                    py: 1,
+                    fontWeight: 100,
+                    borderRadius: '13.516px 13.516px 0px 13.516px',
+                    textAlign: 'right',
+                    direction: 'ltr'
+
                 }}>
-                    Sample message right
+                    Sample message left
                 </Typography>
+                <Avatar sx={{ ml: 2 }} alt="Profile" src="/images/avatar.png" />
             </Box>
 
             <Box sx={{
@@ -25,12 +31,19 @@ const ChatMessages = () => {
                 justifyContent: 'flex-start',
                 p: 1
             }}>
+                <Avatar sx={{ mr: 2 }} alt="Profile" src="/images/avatar.png" />
                 <Typography sx={{
-                    bgcolor: 'grey.300',
-                    p: 1,
-                    borderRadius: 1
+                    bgcolor: '#DEDEE0',
+                    color: '#1D1E2D',
+                    px: 2,
+                    py: 1,
+                    fontWeight: 100,
+                    borderRadius: ' 13.516px 13.516px 13.516px 0px',
+                    textAlign: 'right',
+                    direction: 'ltr'
+
                 }}>
-                    Sample message left
+                    Sample message right
                 </Typography>
             </Box>
 
